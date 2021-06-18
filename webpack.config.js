f3fb19bbf8d4da03a65842afd84c1bd4a037336d
latoyadawson <78005063+latoyadawson@users.noreply.github.com>
2021-06-17 19:48:16 -0700

    opotomization

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,44 @@ const  webpack  = require("webpack");
 //write options within this object that tell webpack what to do (entry, output, mode).
 //a config file is not necessary, but we still want to use one so that we can be more specific with how webpack will function
 
+
+
+
+
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+        filename: '[name].bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.jpg$/i, 
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false,
+                            name(file) {
+                                return "[path][name].[ext]"
+                            },
+                            publicPath: function(url) {
+                                return url.replace("../", "/assets")
+                            }
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader'
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
